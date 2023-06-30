d03c7026fa560ec0ad923fd60f5091aa9f2ab95e
Eric Kwiatkowski <111744309+erickwiatkowski@users.noreply.github.com>
2023-06-30 15:37:55 -0300

    Update medicamento.js

--- app/model/medicamento.js
+++ app/model/medicamento.js
@@ -8,7 +8,7 @@ export class Medicamento{
 }
 
 window.onload = function () {
-    fetch('https://erickwiatkowski.github.io/projrto-2023/db.json')
+    fetch('https://my-json-server.typicode.com/erickwiatkowski/projrto-2023/produtos')
       .then(response => response.json())
       .then(data => {
         const produtos = data || [];
