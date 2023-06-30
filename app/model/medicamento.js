328bededd414fb3c768d31b2c82df2573750930e
Eric Kwiatkowski <111744309+erickwiatkowski@users.noreply.github.com>
2023-06-30 15:32:50 -0300

    Update medicamento.js

--- app/model/medicamento.js
+++ app/model/medicamento.js
@@ -8,33 +8,33 @@ export class Medicamento{
 }
 
 window.onload = function () {
-  fetch('https://erickwiatkowski.github.io/projrto-2023/db.json')
-    .then(response => response.json())
-    .then(data => {
-      const produtos = data || [];
-      let html = '';
-        if (Array.isArray(produtos)) {
-      produtos.forEach(produto => {
-        html += `
-          <div class="row">
-            <div class="col s12 m12">
-              <div class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                  <span class="card-title">${produto.medicamento}</span>
-                  <p>${produto.fabricante}</p>
-                  <p>${produto.data}</p>
+    fetch('https://erickwiatkowski.github.io/projrto-2023/db.json')
+      .then(response => response.json())
+      .then(data => {
+        const produtos = data || [];
+        let html = '';
+          if (Array.isArray(produtos)) {
+        produtos.forEach(produto => {
+          html += `
+            <div class="row">
+              <div class="col s12 m12">
+                <div class="card blue-grey darken-1">
+                  <div class="card-content white-text">
+                    <span class="card-title">${produto.medicamento}</span>
+                    <p>${produto.fabricante}</p>
+                    <p>${produto.data}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        `;
-      });  });
-} else {
-  console.error('Error fetching data: produtos is not an array');
-}
-      document.getElementById('lista-produto').innerHTML = html;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-};
+          `;
+        }); 
+  } else {
+    console.error('Error fetching data: produtos is not an array');
+  }
+        document.getElementById('lista-produto').innerHTML = html;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  };
